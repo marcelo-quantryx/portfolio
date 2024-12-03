5a9d06428f6912c8a4917df474749f55d4934ffc
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-03 21:52:57 +0000

    Update src/components/experience/ExperienceIcon.tsx [BoltSync]

--- src/components/experience/ExperienceIcon.tsx
+++ src/components/experience/ExperienceIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Database, ShoppingCart, Code, Building2 } from 'lucide-react';
+import { Database, ShoppingCart, Code, Building2, BarChart } from 'lucide-react';
 
 interface ExperienceIconProps {
   company: string;
@@ -11,10 +11,12 @@ export function ExperienceIcon({ company }: ExperienceIconProps) {
       return <Database className="w-6 h-6 text-gray-600" />;
     case 'I Saw It First':
       return <ShoppingCart className="w-6 h-6 text-gray-600" />;
-    case 'Freelance':
+    case 'Freelance Data Engineer':
       return <Code className="w-6 h-6 text-gray-600" />;
     case 'SMBC':
       return <Building2 className="w-6 h-6 text-gray-600" />;
+    case 'Banco Itau BBA':
+      return <BarChart className="w-6 h-6 text-gray-600" />;
     default:
       return <Code className="w-6 h-6 text-gray-600" />;
   }
