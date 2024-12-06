7e6baa3f26e2c5bc325f24640773fc722cc7a34b
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-06 18:53:41 +0000

    Update src/components/common/Link.tsx [BoltSync]

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useTracking } from '../../hooks/useTracking';
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
   children: React.ReactNode;
+  category?: string;
 }
 
-export function Link({ href, children, ...props }: LinkProps) {
+export function Link({ href, children, category = 'navigation', ...props }: LinkProps) {
+  const { trackEvent } = useTracking();
+
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
+    
+    // Track the click event
+    trackEvent(category, 'click', href);
     
     // If it's a hash link on the home page, handle smooth scrolling
     if (href.startsWith('/#') && window.location.pathname === '/') {
