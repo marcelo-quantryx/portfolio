947183626d85a1ce35eff0efcf88b5ae6e24300a
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-06 16:08:27 +0000

    Update src/data/education.ts [BoltSync]

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,6 +2,7 @@ import type { Education, Language } from '../types/education';
 
 export const education: Education = {
   institution: "University Centre of FEI",
+  website: "https://portal.fei.edu.br/",
   location: "São Bernardo do Campo, Brazil",
   degree: "Bachelor of Science Production Engineering",
   period: "February 2009 - June 2015",
@@ -9,7 +10,7 @@ export const education: Education = {
 };
 
 export const languages: Language[] = [
-  { name: "Portuguese", level: "C2, Proficient" },
-  { name: "English", level: "C2, Proficient" },
+  { name: "English", level: "C2" },
+  { name: "Portuguese", level: "C2" },
   { name: "Spanish", level: "B2" }
 ];
