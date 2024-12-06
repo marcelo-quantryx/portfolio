aff7d9c34fd9d6e1349bce0adaccbb1ec8b231dd
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-06 18:06:51 +0000

    Update src/data/referrals.ts [BoltSync]

--- src/data/referrals.ts
+++ src/data/referrals.ts
@@ -5,15 +5,15 @@ export const referrals: Referral[] = [
     name: "Igor Venturelli",
     position: "Senior Software Engineer",
     company: "Truelogic Software",
-    image: "images/igor.jpeg",
-    testimonial: "I collaborated with Marcelo on the Liberty Music PR project, where he demonstrated exceptional skills in developing web scraping solutions and API integrations. His ability to architect scalable systems and attention to code quality significantly contributed to the project's success",
+    image: "/images/igor.jpeg",
+    testimonial: "I collaborated with Marcelo on the Liberty Music PR project, where he demonstrated exceptional skills in developing web scraping solutions and API integrations.",
     linkedin: "https://www.linkedin.com/in/igventurelli/"
   },
   {
     name: "Gisele Corraleiro",
     position: "Senior Business Analyst",
     company: "Banco Itaú BBA",
-    image: "images/gisele.jpeg",
+    image: "/images/gisele.jpeg",
     testimonial: "Marcelo is a creative professional who brings innovation to the workplace. His work is consistent and collaborative, based on technical knowledge and thorough research.",
     linkedin: "https://www.linkedin.com/in/gisele-corraleiro-465bb426/"
   }
