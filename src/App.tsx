e9fd6df07ed62f257b7d4815603692f7c84d7c4e
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-06 18:53:37 +0000

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import { Projects } from './components/Projects';
 import { Referrals } from './components/Referrals';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { SEO } from './components/common/SEO';
+import { initGTM } from './utils/gtm';
 
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
   useEffect(() => {
+    // Initialize Google Tag Manager
+    initGTM();
+
     const handleLocationChange = () => {
       setCurrentPath(window.location.pathname);
     };
@@ -43,6 +48,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-white">
+      <SEO />
       <Header />
       <main>
         {renderContent()}
