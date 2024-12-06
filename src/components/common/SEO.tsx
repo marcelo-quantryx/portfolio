869609e952fa3d216a4c9aed0a1a167815201cab
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-06 18:53:40 +0000

    Update src/components/common/SEO.tsx [BoltSync]

---
+++ src/components/common/SEO.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+interface SEOProps {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  type?: string;
+}
+
+export function SEO({
+  title = 'Marcelo Gazzola Ribeiro | Data Engineer',
+  description = 'Data Engineer specializing in cloud-native solutions, ETL pipelines, and data architecture.',
+  image = '/images/me.jpeg',
+  url = 'https://marceloribeiro.dev',
+  type = 'website'
+}: SEOProps) {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </Helmet>
+  );
+}
