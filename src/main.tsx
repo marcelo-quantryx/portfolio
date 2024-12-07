4f7c2666a838fd04974d37851fa06ec7cf825ecb
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-07 18:35:52 -0300

    Update src/main.tsx [BoltSync]

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { AnalyticsProvider } from './components/analytics/AnalyticsProvider';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <AnalyticsProvider>
+        <App />
+      </AnalyticsProvider>
     </HelmetProvider>
   </StrictMode>
 );
