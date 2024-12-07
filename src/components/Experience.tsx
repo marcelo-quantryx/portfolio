da391bcb80b073c93ff6b6321efe770b30a3de05
marcelo-quantryx <143701550+marcelo-quantryx@users.noreply.github.com>
2024-12-07 18:27:31 -0300

    Update src/components/Experience.tsx [BoltSync]

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ExperienceItem } from './experience/ExperienceItem';
+import { ExperienceList } from './experience/ExperienceList';
 import { experiences } from '../data/experience';
 
 export function Experience() {
@@ -7,15 +7,7 @@ export function Experience() {
     <section id="experience" className="py-16 bg-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <h2 className="text-3xl font-bold text-gray-900 mb-8">Experience</h2>
-        <div className="relative">
-          {experiences.map((experience, index) => (
-            <ExperienceItem
-              key={`${experience.name}-${experience.period}`}
-              experience={experience}
-              isLast={index === experiences.length - 1}
-            />
-          ))}
-        </div>
+        <ExperienceList experiences={experiences} />
       </div>
     </section>
   );
